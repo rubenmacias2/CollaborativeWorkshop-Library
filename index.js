e1c380f71fa9ef4ec4d15deec07e9da5af83fdef
rubenmacias2 <rubenmacias2h@gmail.com>
2021-03-02 21:14:01 -0500

    first version proyect

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+const indexRouter = require('./routes/administrator.js');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(port, () => console.log(`Server Listen ${port}`));
